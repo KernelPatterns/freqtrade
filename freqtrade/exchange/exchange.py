4175bd9fba4b7041bb6f15b8d2350acadb66c5f0
eQuantum <8466209+eq19@users.noreply.github.com>
2024-12-30 10:21:44 +0700

    Update exchange.py

--- freqtrade/exchange/exchange.py
+++ freqtrade/exchange/exchange.py
@@ -727,8 +727,8 @@ class Exchange:
         """
         Check if timeframe from config is a supported timeframe on the exchange
         """
-        print("exchange.py 729: self is")
-        pprint(self)
+        self._api.timeframes = self._api.options['timeframes']
+        print("exchange.py 729: self._api.timeframes is", self._api.timeframes)
         if not hasattr(self._api, "timeframes") or self._api.timeframes is None:
             # If timeframes attribute is missing (or is None), the exchange probably
             # has no fetchOHLCV method.
